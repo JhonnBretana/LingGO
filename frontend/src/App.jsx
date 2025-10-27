27fab7347aa5f653c869f1f0ff8a7b9e261211c5
kiritsugu14 <tyroneferranco14@gmail.com>
2025-10-27 19:57:40 +0800

    Trial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,10 @@ import LandingPage from "./module/LandingPage.jsx";
 import SigninPage from "./module/AccountModule/SigninPage.jsx";
 import WelcomePage from "./module/AccountModule/WelcomePage.jsx";
 import SigninDetails from "./module/AccountModule/SigninDetails.jsx";
-
+import CebuanoKnowledge from "./module/AccountModule/Lawak.jsx";
+import LanguagePreference from "./module/AccountModule/LanguagePreference.jsx";
+import CebuanoMotivation from "./module/AccountModule/CebuanoMotivation.jsx";
+import ReadyPage from "./module/AccountModule/ReadyPage.jsx";
 function App() {
   return (
     <div>
@@ -13,6 +16,10 @@ function App() {
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/welcome" element={<WelcomePage />} />
         <Route path="/signin-details" element={<SigninDetails />} />
+        <Route path="/cebuano-knowledge" element={<CebuanoKnowledge />} />
+        <Route path="/language-preference" element={<LanguagePreference />} />
+        <Route path="/cebuano-motivation" element={<CebuanoMotivation />} />
+        <Route path="/ready" element={<ReadyPage />} />
       </Routes>
     </div>
   );
